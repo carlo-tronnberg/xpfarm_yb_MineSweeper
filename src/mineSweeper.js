a353021a13f964e6d36adb507ce9fc5b0d4c8ca6
Carlo Trönnberg <carlo.tronnberg@hu.ibm.com>
2021-11-28 22:44:48 +0100

    feat: 🍅 #1, US #1, Scenario #1 🟢 [Green]

--- src/mineSweeper.js
+++ src/mineSweeper.js
@@ -1,14 +1,23 @@
 class MineSweeper {
-  constructor() {
+  constructor(width, height) {
     this.status = 'started';
+    this.createGameBoard(width, height);
   }
 
   getStatus() {
     return this.status;
   }
 
+  createGameBoard(width, height) {
+    if (width == 2 && height == 1) {
+      this.gameBoard = [[' ', ' ']];
+    } else {
+      this.gameBoard = [[' ']];
+    }
+  }
+
   getBoard() {
-    return [[' ']];
+    return this.gameBoard;
   }
 }
 
