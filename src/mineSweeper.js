3d0a54863f2db64d350a06ca7f18452849284607
Carlo Trönnberg <carlo.tronnberg@hu.ibm.com>
2021-11-28 22:01:21 +0100

    chore: ✨ create dummy files
    Create dummy test and implementation files

---
+++ src/mineSweeper.js
@@ -0,0 +1,12 @@
+class xpfarm_yb_MineSweeper {
+  constructor() {
+    this.status = 'started';
+  }
+
+  getStatus() {
+    return this.status;
+  }
+}
+
+module.exports = { xpfarm_yb_MineSweeper };
+
---
+++ test/mineSweeper.test.js
@@ -0,0 +1,11 @@
+const { xpfarm_yb_MineSweeper } = require('../src/mineSweeper');
+
+describe('xpfarm_yb_MineSweeper', () => {
+  it('Start a new mineSweeper', () => {
+    const mineSweeper = new xpfarm_yb_MineSweeper();
+
+    expect(mineSweeper.getStatus()).toBe('running');
+  });
+  
+});
+
